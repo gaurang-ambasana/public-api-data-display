b8509bba789e8abd37744e90752424a4a780dff7
Gaurang Ambasana <gpambasana@gmail.com>
2021-07-17 18:28:17 +0530

    added table to display data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-function App() {
+const fetchData = async () => {
+  const { data } = await axios.get(`https://randomuser.me/api/?results=20`);
+
+  const { results } = data;
+
+  return results;
+};
+
+const App = () => {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    fetchData().then((res) => setPeople(res));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>Sr</th>
+            <th>Photo</th>
+            <th>Name</th>
+            <th>Age</th>
+            <th>DOB</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {people.map((person, id) => (
+            <tr key={id}>
+              <td>{++id}</td>
+              <td>
+                <img src={person.picture.thumbnail} alt="img" />
+              </td>
+              <td>{`${person.name.title}. ${person.name.first} ${person.name.last}`}</td>
+              <td>{person.dob.age}</td>
+              <td>{person.dob.date.toString().substr(0, 10)}</td>
+              <td>{person.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
   );
-}
+};
 
 export default App;
